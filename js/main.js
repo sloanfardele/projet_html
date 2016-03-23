502f2cfd8fe9219b4eff1abf71ea745f4ec3416d
sloanfardele <sloan.fardele@outlook.fr>
2016-03-23 11:54:19 +0100

    création du truc pour augmenter l atille d'une grille

--- js/main.js
+++ js/main.js
@@ -46,21 +46,24 @@ function initGrille() {
     //initialisation de la grille
     var txt = "";
     txt += "<table>";
+    idtmp=0;
     for (i=0; i<3; i++){
         txt += "<tr>";
         for (j=0; j<3; j++){
             g.caseGGrille[i].posx = i;
             g.caseGGrille[i].posy = j;
-            txt += "<td id='x"+g.caseGGrille[i].posx+"-y"+g.caseGGrille[i].posy+"' class=blanc>";
+            var id="x"+g.caseGGrille[i].posx+"y"+g.caseGGrille[i].posy;
+            txt += "<td id='"+id+"' class=blanc>";
 
 
-            txt += "<table>";
+            idtmp++;
+            txt += "<table id='"+idtmp+"' onclick='choixCasePetiteGrillle("+idtmp+")'>";
             for (k=0;k<3;k++){
                 txt += "<tr>";
                 for (l=0;l<3;l++){
                     g.caseGGrille[i].casePGrille[k].posx = k;
                     g.caseGGrille[i].casePGrille[k].posy = l;
-                    txt += "<td id='x"+g.caseGGrille[i].casePGrille[k].posx+"-y"+g.caseGGrille[i].casePGrille[k].posy+"' class=noir></td>";
+                    txt += "<td id='px"+g.caseGGrille[i].casePGrille[k].posx+"-y"+g.caseGGrille[i].casePGrille[k].posy+"' class=noir></td>";
                 }
                 txt += "</tr>";
             }
@@ -74,18 +77,12 @@ function initGrille() {
     txt += "</table>";
     document.getElementById("plateau").innerHTML += txt;
 
+}
 
-    /*txt = "<table>";
-    txt += "<tr>";
-            for (k=0;k<3;k++){
-                for (l=0;l<3;l++){
-                    g.caseGGrille[i].casePGrille[k].posx = k;
-                    g.caseGGrille[i].casePGrille[k].posy = l;
-                    txt += "<td id='x"+g.caseGGrille[i].casePGrille[k].posx+"-y"+g.caseGGrille[i].casePGrille[k].posy+"' class=noir></td>";
-                }
-            }
-            txt += "</tr>";
-    txt += "</table>";
-
-    document.getElementById("plateau").innerHTML += txt;*/
+function choixCasePetiteGrillle(identif)
+{
+    console.log(identif);
+    var grille = document.getElementById(identif);
+    grille.className+=".grandirGrille";
+    
 }
